0fc0ca96ec3ff7460f3ec5730d4a06a204f81366
Francisco Fernandez Calzado <ffercal0712@g.educaand.es>
2025-04-07 14:27:09 +0200

    Control de versión 2

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones con Git");
+		System.out.println("Buenos días.");
 	}
 
 }
